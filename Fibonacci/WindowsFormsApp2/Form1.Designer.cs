2438203266a23f8f147bc4367cd1079ccdac9858
Stefan-Raul Tuga <tugaraul@yahoo.com>
2026-06-08 02:29:40 +0300

    Add files via upload

---
+++ Fibonacci/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.camp_n = new System.Windows.Forms.TextBox();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n=";
+            // 
+            // camp_n
+            // 
+            this.camp_n.Location = new System.Drawing.Point(39, 46);
+            this.camp_n.Name = "camp_n";
+            this.camp_n.Size = new System.Drawing.Size(116, 20);
+            this.camp_n.TabIndex = 1;
+            this.camp_n.KeyDown += new System.Windows.Forms.KeyEventHandler(this.camp_n_KeyDown);
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(178, 46);
+            this.rez.Multiline = true;
+            this.rez.Name = "rez";
+            this.rez.Size = new System.Drawing.Size(232, 381);
+            this.rez.TabIndex = 2;
+            this.rez.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(39, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Afiseaza";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(422, 454);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.camp_n);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Fibonacci";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox camp_n;
+        private System.Windows.Forms.TextBox rez;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Fibonacci/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double termenFibonacci(int n)
+        {
+            int i;
+            double f0, f1, fn = 0;
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else
+            {
+                f0 = 0;
+                f1 = 1;
+                i = 2;
+                while (i <= n)
+                {
+                    fn = f0 + f1;
+                    f0 = f1;
+                    f1 = fn;
+                    i++;
+                }
+                return fn;
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            try
+            {
+                n = Convert.ToInt16(camp_n.Text);
+                if (n > 500)
+                    MessageBox.Show("Numarul introdus este prea mare.Dati o valoare mai mica decat 500!", "Atentie!");
+                else if (n < 0)
+                    MessageBox.Show("Numarul introdus trebuie sa fie pozitiv!", "Atentie!");
+                else
+                {
+                    rez.Text = "";//se sterg calculele anterioare
+                    for (int i = 0; i <= n; i++)
+                        rez.Text += "Termen = " + i + " Valoare = " + termenFibonacci(i) + "\r\n";
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Trebuie sa introduceti un numar natural!", "Atentie");
+            }
+        }
+
+        private void camp_n_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Return)
+            {
+                this.button1_Click(this.button1, null);
+            }
+        }
+    }
+}
